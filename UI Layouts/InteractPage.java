67b32c0ce67e83be605d63e94ae5f015082ba216
knt5148 <78001055+knt5148@users.noreply.github.com>
2021-02-15 11:15:41 -0500

    Add files via upload

---
+++ UI Layouts/InteractPage.java
@@ -0,0 +1,14 @@
+package com.example.krypty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InteractPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interact_page);
+    }
+}

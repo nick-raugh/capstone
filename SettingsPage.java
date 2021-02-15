96277698b98947af07ef1279861c5987aad9e103
acf5362 <73560559+acf5362@users.noreply.github.com>
2021-02-15 13:54:00 -0500

    Add files via upload

---
+++ SettingsPage.java
@@ -0,0 +1,14 @@
+package com.example.krypty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_page);
+    }
+}

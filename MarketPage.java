da088495c8404e81b01222f1e845f503c8b97a88
acf5362 <73560559+acf5362@users.noreply.github.com>
2021-02-15 13:54:34 -0500

    Market pages

---
+++ MarketPage.java
@@ -0,0 +1,14 @@
+package com.example.krypty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarketPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.market_page);
+    }
+}

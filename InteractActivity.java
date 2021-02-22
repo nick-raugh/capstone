93e2f501469e22eab2eb54d39059f89aaec24d41
knt5148 <78001055+knt5148@users.noreply.github.com>
2021-02-22 17:31:40 -0500

    Add files via upload

---
+++ InteractActivity.java
@@ -0,0 +1,57 @@
+package com.example.krypty;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class InteractActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interact);
+
+        //Initialize
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        //Select Nav Button
+        bottomNavigationView.setSelectedItemId(R.id.interact);
+
+        //ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    //Interact Page
+                    case R.id.interact:
+                        return true;
+
+                    //Market Page
+                    case R.id.market:
+                        startActivity(new Intent(getApplicationContext(),MarketActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    //Home Page
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    //Account Page
+                    case R.id.account:
+                        startActivity(new Intent(getApplicationContext(),AccountActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}

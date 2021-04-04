de4f059cabddf1ba18153413af912f8bbdeb1419
knt5148 <78001055+knt5148@users.noreply.github.com>
2021-04-04 16:51:07 -0400

    Add files via upload

---
+++ ContactsActivity_2.kt
@@ -0,0 +1,87 @@
+package com.coinbase.krypty
+
+import android.Manifest
+import android.content.ContentResolver
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_contacts.*
+
+
+class ContactsActivity : AppCompatActivity() {
+
+    companion object {
+        val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+        loadContacts.setOnClickListener { loadContacts() }
+    }
+
+    private fun loadContacts() {
+        var builder = StringBuilder()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                        Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS),
+                    PERMISSIONS_REQUEST_READ_CONTACTS)
+            //callback onRequestPermissionsResult
+        } else {
+            builder = getContacts()
+            listContacts.text = builder.toString()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadContacts()
+            } else {
+                //  toast("Permission must be granted in order to display contacts information")
+            }
+        }
+    }
+
+    private fun getContacts(): StringBuilder {
+        val builder = StringBuilder()
+        val resolver: ContentResolver = contentResolver;
+        val cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null,
+                null)
+
+        if (cursor.count > 0) {
+            while (cursor.moveToNext()) {
+                val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phoneNumber = (cursor.getString(
+                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))).toInt()
+
+                if (phoneNumber > 0) {
+                    val cursorPhone = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", arrayOf(id), null)
+
+                    if(cursorPhone.count > 0) {
+                        while (cursorPhone.moveToNext()) {
+                            val phoneNumValue = cursorPhone.getString(
+                                    cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            builder.append("Contact: ").append(name).append(", Phone Number: ").append(
+                                    phoneNumValue).append("\n\n")
+                            Log.e("Name ===>",phoneNumValue);
+                        }
+                    }
+                    cursorPhone.close()
+                }
+            }
+        } else {
+            //   toast("No contacts available!")
+        }
+        cursor.close()
+        return builder
+    }
+}
---
+++ RoomAppDb.kt
@@ -0,0 +1,44 @@
+package com.coinbase.krypty.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+
+@Database(entities = [UserEntity::class], version = 2)
+abstract class RoomAppDb: RoomDatabase() {
+
+
+    abstract fun userDao(): UserDao?
+
+    companion object {
+        private var INSTANCE: RoomAppDb?= null
+
+        val migration_1_2: Migration = object: Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE userinfo ADD COLUMN phone TEXT DEFAULT ''")
+            }
+        }
+
+        fun getAppDatabase(context: Context): RoomAppDb? {
+
+            if(INSTANCE == null ) {
+
+                INSTANCE = Room.databaseBuilder<RoomAppDb>(
+                        context.applicationContext, RoomAppDb::class.java, "AppDBB"
+                )
+                        .addMigrations(migration_1_2)
+                        .allowMainThreadQueries()
+                        .build()
+
+            }
+            return INSTANCE
+        }
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
+}

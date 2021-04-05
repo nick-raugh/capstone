eafd85e3714f61284f8ac0671d887e52f6d92b4a
acf5362 <73560559+acf5362@users.noreply.github.com>
2021-04-05 15:20:14 -0400

    Create UserEntity.kt

---
+++ UserEntity.kt
@@ -0,0 +1,14 @@
+package com.coinbase.krypty.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "userinfo")
+data class UserEntity (
+        @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id : Int = 0,
+        @ColumnInfo(name = "name") val name: String,
+        @ColumnInfo(name = "email") val email: String,
+        @ColumnInfo(name = "phone") val phone: String?
+)

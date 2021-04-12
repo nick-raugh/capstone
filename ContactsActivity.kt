64e7ea89b57aae60c3cc31d4bdda9f368668ae0f
acf5362 <73560559+acf5362@users.noreply.github.com>
2021-04-12 04:26:21 -0400

    Update ContactsActivity.kt

--- ContactsActivity.kt
+++ ContactsActivity.kt
@@ -1,58 +1,87 @@
 package com.coinbase.krypty
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
-import android.annotation.SuppressLint
-import android.graphics.Color
-import android.os.Build
-import android.support.annotation.RequiresApi
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import com.coinbase.CoinbaseResponse
-import com.coinbase.network.ApiCall
-import com.coinbase.network.ApiCallAdapterFactory
-import com.coinbase.resources.accounts.Account
-import com.coinbase.resources.prices.Price
-import com.coinbase.resources.prices.PricesApi
-import com.coinbase.resources.prices.PricesApiRx
-import com.coinbase.resources.prices.PricesResource
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.activity_accounts.*
-import kotlinx.android.synthetic.main.activity_currencies.*
-import kotlinx.android.synthetic.main.item_account.view.*
-import java.math.BigDecimal
-import java.math.RoundingMode
-import java.time.Instant.now
-import java.time.LocalDate
-import java.time.LocalDate.now
 import java.util.*
-import kotlin.collections.ArrayList
-
 
 
 class ContactsActivity : AppCompatActivity() {
 
-    private val onDestroyDisposable = CompositeDisposable()
+    private lateinit var dataBase : SqliteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //recyclerView.layoutManager = LinearLayoutManager(this)
+        val contactView: RecyclerView = findViewById(R.id.myContactList)
+        val linearLayoutManager = LinearLayoutManager(this)
+        contactView.layoutManager = linearLayoutManager
+        contactView.setHasFixedSize(true)
+        dataBase = SqliteDatabase(this)
+        var allContacts: ArrayList<Contacts> = dataBase.listContacts()
 
 
+        // determine whether there are contacts to be displayed
+        if(allContacts.size > 0){
+            contactView.visibility= View.VISIBLE
+            val mAdapter = ContactAdapter(this,allContacts)
+            contactView.adapter = mAdapter
+        }
+        else{
+            contactView.visibility=View.GONE
+            //tell user the database is empty
+            Toast.makeText(this,"No contacts stored.",Toast.LENGTH_LONG).show()
+        }
+
+        //add contact button
+        val btnAdd: Button = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener{addTaskDialog()}
     }
+
+    private fun addTaskDialog(){
+        val inflater = LayoutInflater.from(this)
+        val subView = inflater.inflate(R.layout.add_contacts,null)
+        val nameField : EditText = subView.findViewById(R.id.enterName)
+        val noField : EditText = subView.findViewById(R.id.enterPhoneNum)
+        val builder = AlertDialog.Builder(this,R.style.AlertDialogTheme)
+        builder.setTitle("Add contact")
+        builder.setView(subView)
+        builder.create()
+
+        builder.setPositiveButton("ADD CONTACT"){ _, _ ->
+            val name = nameField.text.toString()
+            val phoneNum = noField.text.toString()
+            if(TextUtils.isEmpty(name)){
+                Toast.makeText(this@ContactsActivity,"Oops, something went wrong. Make sure there is a name entered.",Toast.LENGTH_LONG).show()
+            }
+            else{
+                val newContact = Contacts(name, phoneNum)
+                dataBase.addContacts(newContact)
+                finish()
+                startActivity(intent)
+            }
+        }
+
+        builder.setNegativeButton("CANCEL"){ _, _ ->
+            Toast.makeText(this@ContactsActivity,"Add contact cancelled.",Toast.LENGTH_LONG).show()
+        }
+
+        builder.show()
+    }
+
     override fun onDestroy() {
-        onDestroyDisposable.dispose()
         super.onDestroy()
-    }
 
+        //make sure to close the database
+        dataBase.close()
+    }
 }
